78dc56c54476af9e9d7d77d406234c2a7ed847bd
Geet Inder Singh Sodhi <159030760+GeetSodhi@users.noreply.github.com>
2024-06-22 00:08:13 +0530

    Time: 83 ms (47.97%), Space: 62.2 MB (8.67%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,7 +4,7 @@ public:
         int start=0;
         int end=arr.size()-1;
         int mid=start+(end-start)/2;
-        while(start<=end){
+        while(start<end){
             if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]){
                 return mid;
             }
