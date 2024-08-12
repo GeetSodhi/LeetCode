cf1082688cedbe240eddf6722fabd16f7ae779d9
Geet Inder Singh Sodhi <159030760+GeetSodhi@users.noreply.github.com>
2024-08-13 00:20:12 +0530

    Time: 3 ms (97.16%), Space: 8.6 MB (81.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,16 +5,17 @@ public:
         for(int i=0;i<s.size();i++){
             chars[s[i]-'a']=chars[s[i]-'a']+1;
         }
-        int cha[26]={0};
-        for(int i=0;i<t.size();i++){
-            cha[t[i]-'a']=cha[t[i]-'a']+1;
-        }
-      for(int i=0;i<26;i++){
-          if(chars[i]!=cha[i]){
-              return false;
-          }
-      }
-        return true;
         
+        for(int i=0;i<t.size();i++){
+            chars[t[i]-'a']=chars[t[i]-'a']-1;
+        }
+        for(int i=0;i<26;i++){
+            if(chars[i]!=0){
+                return false;
+            }
+        }
+        return true;
+      
+     
     }
 };
