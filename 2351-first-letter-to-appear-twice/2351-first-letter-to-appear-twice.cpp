a487f93887e542ee224662bf30a098873fa865e3
Geet Inder Singh Sodhi <159030760+GeetSodhi@users.noreply.github.com>
2024-08-30 18:28:13 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (56.07%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        int count[26]={0};
+        for(int i=0;i<s.length();i++){
+            count[s[i]-'a']+=1;
+            if((count[s[i]-'a'])==2){
+                return s[i];
+            }
+        }
+        return -1;
+    }
+};
