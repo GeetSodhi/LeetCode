0092d7574f52ce77200bca8d454ba0041a219495
Geet Inder Singh Sodhi <159030760+GeetSodhi@users.noreply.github.com>
2024-08-31 01:08:22 +0530

    Time: 7 ms (84.97%), Space: 14.4 MB (34.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-                vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                ans.push_back(j);
-                    break;
+                unordered_map<int,int> mpp;
+                for(int i=0;i<nums.size();i++){
+                    int MoreNeed=target-nums[i];
+                    if(mpp.find(MoreNeed)!=mpp.end()){
+                        return {mpp[MoreNeed],i};
+                    }
+                    mpp[nums[i]]=i;
                 }
-            }
-        }
-        return ans;
+        return {-1,-1};
     }
 };
