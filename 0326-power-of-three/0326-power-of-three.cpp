e48fcfef540812792bd1c6f970ed392a894ad780
Geet Inder Singh Sodhi <159030760+GeetSodhi@users.noreply.github.com>
2024-09-18 15:27:10 +0530

    Time: 8 ms (79.81%), Space: 8.5 MB (27.99%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -5,6 +5,9 @@ public:
         if(n<=0){
             return false;
         }
-       return 1162261467 % n == 0;
+        if(n==1){
+            return true;
+        }
+      return n%3==0 && isPowerOfThree(n/3);
     }
 };
